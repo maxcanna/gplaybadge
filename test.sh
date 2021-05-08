a624d3b01d31e98773b6a3751df6d921f6606328
Massimiliano Cannarozzo <maxcanna@gmail.com>
2021-05-08 17:39:08 +0200

    Move setup into yaml

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 #! /bin/bash
 
-printf "Installing composer\n"
-curl -sS https://getcomposer.org/installer | php
-
-printf "Installing dependencies\n"
-php composer.phar install --ignore-platform-reqs
-
 printf "Starting server\n"
 php -S 0.0.0.0:3003 -t web/ &
 sleep 3
